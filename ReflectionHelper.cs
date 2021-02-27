9cab568f10600b65021d0d72a44289d036ea5ecc
parmanivivek <63196590+parmanivivek@users.noreply.github.com>
2021-02-27 13:47:07 +0700

    reflection helper feature

---
+++ ReflectionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace ReflectionUtility
+{
+    public class ReflectionHelper
+    {
+        public static object GetPropertyValue(object src, string propName)
+        {
+            if (src == null) return null;
+            if (propName == null) return null;
+
+            if (propName.Contains("."))//complex type nested
+            {
+                var temp = propName.Split(new char[] { '.' }, 2);
+                return GetPropertyValue(GetPropertyValue(src, temp[0]), temp[1]);
+            }
+            else
+            {
+                var prop = src.GetType().GetProperty(propName);
+                return prop != null ? prop.GetValue(src, null) : null;
+            }
+        }
+
+        public static PropertyInfo GetPropertyInfo(object src, string propName)
+        {
+            if (src == null) return null;
+            if (propName == null) return null;
+
+            if (propName.Contains("."))//complex type nested
+            {
+                var temp = propName.Split(new char[] { '.' }, 2);
+                return GetPropertyInfo(GetPropertyValue(src, temp[0]), temp[1]);
+            }
+            else
+            {
+                var prop = src.GetType().GetProperty(propName);
+                return prop;
+            }
+        }
+    }
+}
